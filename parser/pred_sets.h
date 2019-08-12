a8b586dd8eba5c108f6c590ae996e06b5c051e69
giovannirosario <giovanni.rosario@hotmail.com>
2019-08-12 10:32:56 -0300

    first set types

--- parser/pred_sets.h
+++ parser/pred_sets.h
@@ -25,6 +25,10 @@
 
 #define F_BLK_STMT(x) x == TOK_BOOLEAN || x == TOK_INT || x == TOK_ID || x == TOK_VOID || \
                       x == '{' || x == TOK_WHILE || x == TOK_PRINT || x == TOK_ID || x == TOK_CONTINUE || \
-                      x == TOK_BREAK || x == TOK_RETURN || x == ';' || x == TOK_IF \
+                      x == TOK_BREAK || x == TOK_RETURN || x == ';' || x == TOK_IF || x == EPS
+
+#define F_TYPE(x) x == TOK_BOOLEAN || x == TOK_INT || x == TOK_ID || x == TOK_VOID
+
+#define F_TYPE1(x) x == '[' || x == EPS
 
 #endif
