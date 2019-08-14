7911a0a55020fd10466ac367b74ff8a34e2da54b
Josenaldo <josenaldo.asj@gmail.com>
2019-08-14 06:34:01 -0300

    Finished FIRST set for expressions

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 #include "pred_sets.h"
 
 int main(){
-    printf("%i\n", F_E2('-'));
+    printf("%i\n", FR_E('9'));
     return 0;
 }
--- parser/pred_sets.h
+++ parser/pred_sets.h
@@ -11,16 +11,17 @@
 #define F_T(x) F_E1(x)
 #define F_F(x) F_E1(x)
 #define F_Un(x) x == '-' || x == '!'
-#define F_ElP(x) x == ',' || x == EPS
+#define F_Elp(x) x == ',' || x == EPS
 #define F_El(x) F_E1(x) || x == EPS
 #define F_R(x) x == '{' || x == '(' || x == '-' || x == '!'
 #define F_t(x) x == TOK_LIT_INT || x == TOK_TRUE || x == TOK_FALSE || x == TOK_ID || \
-        x == TOK_LIT_STR || x == TOK_THIS ||x == TOK_NULL ||x == '{' ||x == '(' ||x == '-' || \
-        x == '!'
+        x == TOK_LIT_STR || x == TOK_THIS ||x == TOK_NULL 
 #define F_Relop(x) x == '<' || x == TOK_LESS_EQ || x == '>' || x == TOK_GREAT_EQ || x == TOK_EQ || \
         x == TOK_DIFF
 #define F_Multop(x) x == '/' || x == '*' || x == '%'
 #define F_Boolop(x) x == TOK_AND || x == TOK_OR
+#define F_Addop(x) x == '+' || x == '-'
+#define F_X(x) x == '.' || x == '['
 
 // E -> E1 E’
 #define FR_E(x) F_E1(x)
@@ -28,16 +29,16 @@
 // E' -> rel_op E1
 #define FR_Ep_1(x) F_Relop(x) 
 
-// E' -> eps
+// E' -> EPS
 #define FR_Ep_2(x) x == EPS
 
 // E1  -> E2 E1'
-#define FR_E1(x) F_E(2)
+#define FR_E1(x) F_E2(x)
 
 // E1' -> bool_op E2 E1'
 #define FR_E1p_1(x) F_Boolop(x)
 
-// E1' -> eps
+// E1' -> EPS
 #define FR_E1p_2(x) x == EPS
 
 // E2  -> E3 E2'
@@ -46,56 +47,84 @@
 // E2' -> mult_op E3 E2'
 #define FR_E2p_1(x) F_Multop(x)
 
-// E2' -> eps
+// E2' -> EPS
 #define FR_E2p_2(x) x == EPS
 
 // E3  -> T E3'
 #define FR_E3(x) F_T(x)
 
 // E3' -> add_op T E3'
-#define FR_E3p_1(x) x == '+' || x == '-'
+#define FR_E3p_1(x) F_Addop(x)
 
-// E3' -> eps
+// E3' -> EPS
 #define FR_E3p_2(x) x == EPS
 
 // T -> F T’ 
 #define FR_T(x) F_F(x)
 
 // T’ -> X T’
-#define FR_Tp_1(x) 
+#define FR_Tp_1(x) F_X(x)
 
-/*
-E -> E1 E’
-E’ -> rel_op E1 | EPSILON
-E1 -> E2 E1'
-E1' -> bool_op E2 E1' | EPSILON
-E2 -> E3 E2'
-E2' -> mult_op E3 E2' | EPSILON
-E3 -> T E3'
-E3' -> add_op T E3' | EPSILON
+// T2 -> , E
+#define FR_T2_1(x) x == ','
 
-rel_op    -> < | <= | > | >= | == | !=
-bool_op   -> && | ||
-mult_op   -> / | * | %
-add_op      -> + | -
+// T2 -> EPS
+#define FR_T2_2(x) x == EPS
 
-T -> F T’
-T’-> X T’ | EPSILON
-T2 -> , E | EPSILON                   
-T3 -> E T4 | EPSILON
-T4 -> , E T4 | EPSILON
-X -> . X’ | [ E ]
-X’ -> length P | substring ( E T2 ) | ID ( T3 )
-F -> t | R
-t -> LIT_INT | TRUE | FALSE | ID | LIT_STR | THIS | NULL
+// T3 -> E T4
+#define FR_T3_E1(x) F_E(x)
 
-R -> { El } | Un T | ( E )
-El -> E El’ | EPSILON
-El’-> , E El’ | EPSILON
+// T3 -> EPS
+#define FR_T3_E2(x)  x == EPS
 
-Un -> - | !
+// T4 -> , E T4
+#define FR_T4_1(x) x == ','
 
-*/
+// T3 -> EPS
+#define FR_T4_E2(x)  x == EPS
+
+// X -> . X’
+#define FR_X_1(x) x == ','
+
+// X -> [ E ]
+#define FR_X_2(x) x == '['
+
+// X' -> length P
+#define FR_Xp_1(x) x == TOK_LENGTH
+
+// X' -> substring ( E T2 )
+#define FR_Xp_2(x) x == TOK_SUBSTRING
+
+// X' -> ID ( T3 )
+#define FR_Xp_3(x) x == TOK_ID
+
+// F -> t
+#define FR_F_1(x) F_t(x)
+
+// F -> R
+#define FR_F_2(x) F_R(x)
+
+// R -> { El }
+#define FR_R_1(x) x == '{'
+
+// R -> Un T
+#define FR_R_2(x) F_Un(x)
+
+// R -> ( E )
+#define FR_R_3(x) x == '('
+
+// El -> E El’
+#define FR_El_1(x) FR_E(x)
+
+// El -> EPS
+#define FR_El_2(x)  x == EPS
+
+// El' -> , E El'
+#define FR_Elp_1(x) x == ','
+
+// El' -> EPS
+#define FR_Elp_2(x) x == EPS
+
 #define F_STMT(x) x == '{' || x == TOK_WHILE || x == TOK_PRINT || x == TOK_ID || x == TOK_CONTINUE || \
                   x == TOK_BREAK || x == TOK_RETURN || x == ';' || x == TOK_IF
 
