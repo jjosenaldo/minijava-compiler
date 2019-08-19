6d2f17bf2e3663b47500ed2cc0992374b1d9f563
Josenaldo <josenaldo.asj@gmail.com>
2019-08-19 08:10:44 -0300

    Add some FIRST sets for nonterminals of expressions

--- parser/pred_sets.h
+++ parser/pred_sets.h
@@ -3,127 +3,144 @@
 
 #include "token.h"
 
-#define F_E1(x) x == TOK_LIT_INT || x == TOK_TRUE || x == TOK_FALSE || x == TOK_ID ||\
+// unop litint true false id litstr this null lbrace (
+#define F_E(x) F_Unop(x) || x == TOK_LIT_INT || x == TOK_TRUE || x == TOK_FALSE || x == TOK_ID ||\
         x == TOK_LIT_STR || x == TOK_THIS || x == TOK_NULL || x == '{' || \
-        x == '(' || x == '-' || x == '!'
-#define F_E2(x) F_E1(x)
-#define F_E3(x) F_E1(x)
-#define F_T(x) F_E1(x)
-#define F_F(x) F_E1(x)
-#define F_Un(x) x == '-' || x == '!'
+        x == '(' 
+
+// relop
+#define F_Ep(x) F_Relop(x) || x == EPS
+
+// unop litint true false id litstr this null lbrace (
+#define F_E1(x) F_E(x)
+
+// boolop
+#define F_E1p(x) F_Boolop(x) || x == EPS
+
+// unop litint true false id litstr this null lbrace (
+#define F_E2(x) F_E(x)
+
+// addop
+#define F_E2p(x) F_Addop(x) || x == EPS
+
+// unop litint true false id litstr this null lbrace (
+#define F_E3(x) F_E(x)
+
+// multop
+#define F_E3p(x) F_Multop(x) || x == EPS
+
+// unop litint true false id litstr this null lbrace (
+#define F_T(x) F_E(x)
+
+// dot lbracket
+#define F_Tp(x) x == '.' || x == '{' || x == EPS
+
+// comma
+#define F_T2(x) x == ',' || x == EPS
+
+// unop litint true false id litstr this null lbrace (
+#define F_T3(x) F_E(x) || x == EPS
+
+// comma
+#define F_T4(x) x == ',' || x == EPS
+
+// dot lbracket
+#define F_X(x) x == '.' || x == '['
+
+// length substring id
+#define F_Xp(x) x == TOK_LENGTH || x == TOK_SUBSTRING || x == TOK_ID
+
+// ()
+#define F_P(x) x == '(' || x == ')' || x == EPS
+
+// 	litint true false id litstr this null lbrace (
+#define F_F(x) x == TOK_LIT_INT || x == TOK_TRUE || x == TOK_FALSE || x == TOK_ID || x == TOK_LIT_STR || \
+        x == TOK_THIS || x == TOK_NULL || x == '{' || x == '('
+
+// litint true false id litstr this null
+#define F_TT(x) x == LIT_INT || x == TOK_TRUE || x == TOK_FALSE || x == TOK_ID || x == TOK_LIT_STR || \
+        x == TOK_THIS || x == TOK_NULL
+
+// lbrace (    
+#define F_R(x) x == '{' || x == '('
+
+// unop litint true false id litstr this null lbrace (
+#define F_El(x) F_E(x) || x == EPS
+
+// comma
 #define F_Elp(x) x == ',' || x == EPS
-#define F_El(x) F_E1(x) || x == EPS
-#define F_R(x) x == '{' || x == '(' || x == '-' || x == '!'
-#define F_t(x) x == TOK_LIT_INT || x == TOK_TRUE || x == TOK_FALSE || x == TOK_ID || \
-        x == TOK_LIT_STR || x == TOK_THIS ||x == TOK_NULL 
+ 
+#define F_Unop(x) x == '-' || x == '!'
 #define F_Relop(x) x == '<' || x == TOK_LESS_EQ || x == '>' || x == TOK_GREAT_EQ || x == TOK_EQ || \
         x == TOK_DIFF
 #define F_Multop(x) x == '/' || x == '*' || x == '%'
 #define F_Boolop(x) x == TOK_AND || x == TOK_OR
 #define F_Addop(x) x == '+' || x == '-'
-#define F_X(x) x == '.' || x == '['
 
-// E -> E1 E’
+// E -> E1 Ep .
 #define FR_E(x) F_E1(x)
 
-// E' -> rel_op E1
-#define FR_Ep_1(x) F_Relop(x)
+// Ep -> relop E1 | .
+// E1 -> E2 E1p .
+// E1p -> boolop E2 E1p | .
+// E2 -> E3 E2p .
+// E2p -> addop E3 E2p | .
+// E3 -> T E3p .
+// E3p -> multop T E3p | .
+// T -> F Tp | unop T .
+// Tp-> X Tp | .
+// T2 -> comma E | .                   
+// T3 -> E T4 | .
+// T4 -> comma E T4 | .
+// X -> dot Xp | lbracket E rbracket .
+// Xp -> length P | substring ( E T2 ) | id ( T3 ) .
+// P -> () | .
+// F -> TT | R .
+// TT -> litint | true | false | id | litstr | this | null .
+// R -> lbrace El rbrace | ( E ) .
+// El -> E Elp | .
+// Elp -> comma E Elp | .
 
-// E' -> EPS
-#define FR_Ep_2(x) x == EPS
 
-// E1  -> E2 E1'
-#define FR_E1(x) F_E2(x)
 
-// E1' -> bool_op E2 E1'
-#define FR_E1p_1(x) F_Boolop(x)
 
-// E1' -> EPS
-#define FR_E1p_2(x) x == EPS
+/*
+Goal -> MainClass ClassDeclarations .
+MainClass -> class id lbrace void main ( string lbracket rbracket id ) lbrace Blockstatements rbrace rbrace .
+Blockstatements -> BlockStatement Blockstatements | .
+BlockStatement -> NonclassVarDec | Statement | id AfterId .
 
-// E2  -> E3 E2'
-#define FR_E2(x) F_E3(x)
+ClassDeclarations -> ClassDeclaration ClassDeclarations  | .
+ClassDeclaration -> class id Extends ClassBody .
+Extends -> extends id | .
+ClassBody -> lbrace ClassContent rbrace .
+ClassContent -> ClassComponent ClassContent | .
 
-// E2' -> mult_op E3 E2'
-#define FR_E2p_1(x) F_Multop(x)
+ClassComponent -> Type id RestDec .
 
-// E2' -> EPS
-#define FR_E2p_2(x) x == EPS
+RestDec -> semicolon | ( ParamsOpt ) lbrace Blockstatements rbrace .
+VarDec -> NonclassVarDec |  VarObjDec .
+NonclassVarDec -> NonclassType id semicolon .
+VarObjDec -> id id semicolon .
 
-// E3  -> T E3'
-#define FR_E3(x) F_T(x)
+ParamsOpt -> Params | .
+Params -> Param ParamsRest .
+ParamsRest -> comma Params | .
+Param -> Type id .
 
-// E3' -> add_op T E3'
-#define FR_E3p_1(x) F_Addop(x)
+Type1 -> lbracket rbracket Type1 | .
+NonclassType -> boolean Type1 | int Type1 | void .
+IdType -> id Type1 .
+Type -> NonclassType | IdType .
 
-// E3' -> EPS
-#define FR_E3p_2(x) x == EPS
+Statement ->  lbrace Blockstatements rbrace |  while ( E ) Statement | system dot out dot println ( E ) semicolon  | continue semicolon | break semicolon | return E semicolon | semicolon | if E then Statement OptEelse .
 
-// T -> F T’
-#define FR_T(x) F_F(x)
+OptElse -> else Statement | .
 
-// T’ -> X T’
-#define FR_Tp_1(x) F_X(x)
-
-// T2 -> , E
-#define FR_T2_1(x) x == ','
-
-// T2 -> EPS
-#define FR_T2_2(x) x == EPS
-
-// T3 -> E T4
-#define FR_T3_E1(x) F_E(x)
-
-// T3 -> EPS
-#define FR_T3_E2(x)  x == EPS
-
-// T4 -> , E T4
-#define FR_T4_1(x) x == ','
-
-// T3 -> EPS
-#define FR_T4_E2(x)  x == EPS
-
-// X -> . X’
-#define FR_X_1(x) x == ','
-
-// X -> [ E ]
-#define FR_X_2(x) x == '['
-
-// X' -> length P
-#define FR_Xp_1(x) x == TOK_LENGTH
-
-// X' -> substring ( E T2 )
-#define FR_Xp_2(x) x == TOK_SUBSTRING
-
-// X' -> ID ( T3 )
-#define FR_Xp_3(x) x == TOK_ID
-
-// F -> t
-#define FR_F_1(x) F_t(x)
-
-// F -> R
-#define FR_F_2(x) F_R(x)
-
-// R -> { El }
-#define FR_R_1(x) x == '{'
-
-// R -> Un T
-#define FR_R_2(x) F_Un(x)
-
-// R -> ( E )
-#define FR_R_3(x) x == '('
-
-// El -> E El’
-#define FR_El_1(x) FR_E(x)
-
-// El -> EPS
-#define FR_El_2(x)  x == EPS
-
-// El' -> , E El'
-#define FR_Elp_1(x) x == ','
-
-// El' -> EPS
-#define FR_Elp_2(x) x == EPS
+ArrAssignment -> id  RemainingArrAssignment .
+RemainingArrAssignment -> lbracket E rbracket equal E | equal E .
+AfterId -> RemainingArrAssignment | id semicolon .
+*/
 
 #define F_STMT(x) x == '{' || x == TOK_WHILE || x == TOK_PRINT || x == TOK_ID || x == TOK_CONTINUE || \
                   x == TOK_BREAK || x == TOK_RETURN || x == ';' || x == TOK_IF
