0186ba54647c1ed60a40c56c969e14b4feec9141
Josenaldo <josenaldo.asj@gmail.com>
2019-10-07 19:24:32 -0300

    Fix bug when adding a symbol to the table
    The symbol was always being added at the first position.

--- symtable/symtable.c
+++ symtable/symtable.c
@@ -13,7 +13,7 @@ TableEntryList* createSymTable(){
 
 void addEntryToTable(TableEntryList* table, TableEntry* entry){
 	int position = hashString(entry->id);
-	addEntryToList(table, entry);
+	addEntryToList(table + position, entry);
 }
 
 void removeEntryFromTable(TableEntryList* table, char* id){
@@ -117,4 +117,5 @@ int hashString(char *key){
     // hash += (hash << 15);
     // return hash;
     return key[0] % TABLE_SIZE;
+    // return 4;
 }
