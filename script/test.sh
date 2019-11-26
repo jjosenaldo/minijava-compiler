227f3789d4da5c592c434c316bdda3c507e22d4c
Josenaldo <josenaldo.asj@gmail.com>
2019-11-26 00:15:59 -0300

    Remove unnecessary stuff

--- script/test.sh
+++
@@ -1,5 +0,0 @@
-for filename in ./code-examples/*; do
-	echo -e "------ TEST --------------------------\nFile: $filename\n";
-	./bin/main.out < "$filename";
-	echo -e "--------------------------------------\n\n";
-done
